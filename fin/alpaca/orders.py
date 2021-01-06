6174920bf697bb68647927cd4bb5dcc963aff282
Joshua B. Bussdieker <jbussdieker@gmail.com>
2021-01-05 21:28:58 -0900

    Add more alpaca API endpoints

---
+++ fin/alpaca/orders.py
@@ -0,0 +1,4 @@
+from fin.alpaca.client import Client
+
+def fetch():
+    return Client().request("orders")
---
+++ tests/alpaca/test_account.py
@@ -0,0 +1,18 @@
+import unittest
+import vcr
+import requests
+
+from unittest.mock import MagicMock, patch
+
+from fin.alpaca.account import fetch
+
+iexvcr = vcr.VCR(path_transformer=vcr.VCR.ensure_suffix('.yaml'),
+                 cassette_library_dir='tests/fixtures/cassettes/alpaca/account',
+                 decode_compressed_response=True,
+                 filter_headers=['APCA-API-KEY-ID', 'APCA-API-SECRET-KEY'])
+
+class TestAlpacaAccount(unittest.TestCase):
+    @iexvcr.use_cassette()
+    def test_fetch(self):
+        resp = fetch()
+        self.assertEqual(len(resp), 26)
