fdd818ec855d9d20521a969107c972d5e9d5d1c6
Joshua B. Bussdieker <jbussdieker@gmail.com>
2021-01-18 22:32:03 -0900

    Support additional params for bars

--- fidal/alpaca/bars.py
+++ fidal/alpaca/bars.py
@@ -1,7 +1,14 @@
 from fidal.alpaca.client import Client
 
-def fetch(symbols, timespan='day', limit=10):
+def fetch(symbols, timespan='day', limit=None, start=None, end=None, from_=None, until=None):
     client = Client()
     client.base_url = "https://data.alpaca.markets"
     client.version = "v1"
-    return client.request("bars/{timespan}".format(timespan=timespan), {"limit": limit, "symbols": symbols})
+    return client.request("bars/{timespan}".format(timespan=timespan), {
+        "limit": limit,
+        "symbols": symbols,
+        "start": start,
+        "end": end,
+        "from": from_,
+        "until": until,
+    })
