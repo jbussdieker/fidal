24701815a74709f665606f30ea39de39c76581ce
Joshua B. Bussdieker <jbussdieker@gmail.com>
2021-01-20 00:52:23 -0900

    Update IEX time-series support

--- fidal/iex/time_series.py
+++ fidal/iex/time_series.py
@@ -1,4 +1,6 @@
 from fidal.iex.client import Client
 
-def fetch():
-    return Client().request("time-series")
+def fetch(rec_id=None, rec_key=None, **kwargs):
+    if rec_id is None or rec_key is None:
+        return Client().request("time-series", kwargs)
+    return Client().request("time-series/%s/%s" % (rec_id, rec_key), kwargs)
